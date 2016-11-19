d61d1fabb29a4a7a7fc65d94d7694c0facaa58ac
Nyhven <delaun.d@gmail.com>
2016-11-19 14:23:02 +0100

    plop

--- NewsWatcherAgent/scripts/onContent.js
+++ NewsWatcherAgent/scripts/onContent.js
@@ -2,29 +2,36 @@
  * Created by Nyhven on 19/11/2016.
  */
 
-// alert(new RegExp("http[s]?:\\/\\/([a-z0-9\\.-]*\\.)?facebook.com\\/").test("https://www1.fr-zqd.facebook.com/"));
+// if (new RegExp("http[s]?:\\/\\/([a-z0-9\\.-]*\\.)?"+ "facebook.com" +"\\/").test(window.location.href)) {
 
-var test = "facebook.com";
-var matcher = new RegExp("http[s]?:\\/\\/([a-z0-9\\.-]*\\.)?"+ test +"\\/");
+var facebookMatcher = new RegExp("http[s]?:\\/\\/([a-z0-9\\.-]*\\.)?"+ "facebook.com" +"\\/");
 
-jQuery(".userContentWrapper").each(function(index) {
-    var that = this;
-    var isFake = false;
-
-    $(this).find("a").each(function(index){
-        var href = $(this).attr('href');
-        if (matcher.test(href)) {
-            var linkIsFake = (href.indexOf('remhq.com') !== -1);
-
-            if (linkIsFake) {
-                isFake = true;
-            }
-        }
-    })
-
-    if (isFake) {
-        $(this).find('.comment_link').each(function(index){
-            $(this).text('Let people know this is fake!');
-        });
+setInterval(function() {
+    if (facebookMatcher.test(window.location.href)) {
+        console.log("Facebook...");
+        reload();
     }
-});
+}, 2000);
+
+var reload = function() {
+    jQuery(".userContentWrapper").each(function(index) {
+        console.log("ContentWrapper: ");
+
+        var isFake = false;
+
+        $(this).find("a").each(function(index){
+            var href = $(this).attr('href');
+                var linkIsFake = (href.indexOf('remhq.com') !== -1);
+
+                if (linkIsFake) {
+                    isFake = true;
+                }
+        });
+
+        if (isFake) {
+            $(this).find('.comment_link').each(function(index){
+                $(this).text('Let people know this is fake!');
+            });
+        }
+    });
+}
