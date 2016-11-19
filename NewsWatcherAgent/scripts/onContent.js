d59e3b415042472ac09ad74a8e361e44f428e212
Martin Rolph <martinalexrolph@gmail.com>
2016-11-19 15:26:14 +0100

    Refreshing check and random truth

--- NewsWatcherAgent/scripts/onContent.js
+++ NewsWatcherAgent/scripts/onContent.js
@@ -6,32 +6,42 @@
 
 var facebookMatcher = new RegExp("http[s]?:\\/\\/([a-z0-9\\.-]*\\.)?"+ "facebook.com" +"\\/");
 
-setInterval(function() {
+window.setInterval(function() {
     if (facebookMatcher.test(window.location.href)) {
-        console.log("Facebook...");
         reload();
     }
 }, 2000);
 
 var reload = function() {
     jQuery(".userContentWrapper").each(function(index) {
-        console.log("ContentWrapper: ");
-
-        var isFake = false;
-
-        $(this).find("a").each(function(index){
+        var post = this;
+        $(this).parent().find("a:not(._newswatcher_checked)").each(function(index){
             var href = $(this).attr('href');
-                var linkIsFake = (href.indexOf('remhq.com') !== -1);
-
-                if (linkIsFake) {
-                    isFake = true;
-                }
+            getLinkTruth(href, linkTruthCallback(post, href));
+            $(this).addClass("_newswatcher_checked");
         });
+    });
+}
 
-        if (isFake) {
-            $(this).find('.comment_link').each(function(index){
-                $(this).text('Let people know this is fake!');
+function getLinkTruth(url, callback) {
+    console.log("getting link truth");
+    console.log(url);
+    window.setTimeout(function() {
+        callback({truth: Math.floor(Math.random()*100)});
+    }, 200);
+}
+
+
+function linkTruthCallback(userContentWrapper, url) {
+    return function(results) {
+        if (results.truth < 20) {
+            $(userContentWrapper).find('.comment_link').each(function(index){
+                $(this).text('Comment (untrustworthy article)');
+            });
+        } else if (results.truth < 40) {
+            $(userContentWrapper).find('.comment_link').each(function(index){
+                $(this).text('Comment (article might be fake)');
             });
         }
-    });
+    } 
 }
