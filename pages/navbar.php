8e0567212cc927168e815e7bf23a671dd8f3293d
PLISSON Luc <lplisson@itsgroup.fr>
2016-11-20 00:50:47 +0100

    front

---
+++ pages/navbar.php
@@ -0,0 +1,4 @@
+<li><a href="3dprinting.html">Trusted News</a></li>|
+<li><a href="materials.html">Fake News</a></li>|
+<li><a href="printing.html">Joke News</a></li>|
+<li><a href="filestoprint.html">Misleading News</a></li>
