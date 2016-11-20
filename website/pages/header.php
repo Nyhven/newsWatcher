8782359157c1aa3d471f4467c61052374b76c4f9
soulslider <nuke802.11@gmail.com>
2016-11-20 14:11:24 +0100

    front

--- website/pages/header.php
+++ website/pages/header.php
@@ -1,19 +1,28 @@
 <div class="header">
-    <div class="container">
-        <div class="logo">
+    <div class="container bodycontainer">
+        <div class="logo col-md-8" style='padding-top:0'>
             <a href="index.php">
-                <img src="images/NW-TITLE-LOGO-2-11.png" alt=""/>
-                <h1>News Watcher</h1></a>
+			<div>
+			   <div class="col-md-2">
+					<img src="images/NW-TITLE-LOGO-2-11.png" alt=""/>
+			   </div>
+			   <div class="col-md-10">
+					<div style='padding-top: 0.5em'>
+						<img src="images/NW-TITLE-LOGO-TEXT-12.png" alt=""/>
+					</div>
+					<div class="moto">
+						<span>Your personnal news trusting indicator</span>
+					</div>
+			   </div>
+			</div>
+			</a>
         </div>
         <div class="navigation">
-            <ul>
+            <ul style="padding-right: 6%;padding-top:5%">
                 <?php
                 include 'pages/about.php'
                 ?>
             </ul>
-        </div>
-        <div class="moto">
-            <span>Your personnal news trusting indicator</span>
         </div>
         <div class="clearfix"></div>
     </div>
